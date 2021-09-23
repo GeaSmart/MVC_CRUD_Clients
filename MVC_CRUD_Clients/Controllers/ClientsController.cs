0442f4bef6c0c81f1d799d5761a9aa9c9daa895e
Gerson Azabache <gerson_dunk@hotmail.com>
2021-09-22 19:39:36 -0500

    custimizing views

---
+++ MVC_CRUD_Clients/Controllers/ClientsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_CRUD_Clients.Entities;
+using MVC_CRUD_Clients.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_CRUD_Clients.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private readonly ApplicationDBContext context;
+
+        public ClientsController(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {            
+            return View();
+        }
+
+        public IActionResult AddEdit(int id=0)
+        {
+            return View(new Client());
+        }
+    }
+}
